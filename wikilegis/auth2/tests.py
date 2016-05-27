2b1bdcbb76f617444cd7b0d48c4df53c7a98c9ba
Ruyther Costa <ruy_fusion@hotmail.com>
2016-05-27 16:09:36 -0300

    Some models tests for auth2 app

--- wikilegis/auth2/tests.py
+++ wikilegis/auth2/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from templatetags import auth2
-import models
 from django.test.client import Client
+from wikilegis.auth2.models import User
+from wikilegis.auth2.models import UserManager
 
 
 class TestLogin(TestCase):
@@ -10,3 +10,53 @@ class TestLogin(TestCase):
         client = Client()
         response = client.get('/accounts/login/')
         self.failUnlessEqual(response.status_code, 200)
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.user = User()
+        self.user_manager = UserManager()
+
+    def test_get_full_name_when_the_full_name_is_informed(self):
+        self.user.first_name = "First"
+        self.user.last_name = "Last"
+        self.assertEqual(self.user.get_full_name(), "First Last")
+
+    def test_get_full_name_when_the_first_name_is_informed_empty(self):
+        self.user.first_name = ""
+        self.user.last_name = "Last"
+        self.assertEqual(self.user.get_full_name(), "Last")
+
+    def test_get_full_name_when_the_last_name_is_informed_empty(self):
+        self.user.first_name = "First"
+        self.user.last_name = ""
+        self.assertEqual(self.user.get_full_name(), "First")
+
+    def test_get_short_name_when_is_informed_the_first_name(self):
+        self.user.first_name = "First"
+        self.assertEqual(self.user.get_short_name(), "First")
+
+    def test_get_display_name_when_is_informed_the_email(self):
+        self.user.email = "test@test.com"
+        self.assertEqual(self.user.get_display_name(), "test@test.com")
+
+    def test_get_display_name_when_is_not_informed_the_email(self):
+        self.user.email = ""
+        self.assertEqual(self.user.get_display_name(), "")
+
+    def test_get_display_name_when_is_informed_the_full_name(self):
+        self.user.first_name = "First"
+        self.user.last_name = "Last"
+        self.user.get_full_name()
+        self.assertEqual(self.user.get_display_name(), "First Last")
+
+    def test_unicode_when_is_informed_the_display_name(self):
+        self.user.email = "test@test.com"
+        self.user.get_display_name()
+        self.assertEqual(self.user.__unicode__(), "test@test.com")
+
+    def test_get_absolute_url_when_email_is_not_informed(self):
+        with self.assertRaises(ValueError) as context:
+            self.user_manager._create_user("", 12345, False, False)
+        self.assertTrue('The given email must be set' in context.exception)
